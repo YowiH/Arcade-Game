c5d88e9ee6b2e9aec2e617e4c47ec77287237587
kirstennp <k1rsten5neubauerpons@gmail.com>
2025-05-05 12:12:45 +0200

    Restoring past code + adding new code
    I dont know what happene but my code got lost so i just restored assets and some code

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,39 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 #include <vector>
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-Vector2 player_size{ 16, 16 };
-Vector2 player_pos{ 0, 0};
-int player_Speed = 5;
+const float tile_size = 32;
+const float area_size = tile_size * 16;
+const int NUMBER_OF_TILES = 256;
+
+Vector2 player_size{ tile_size, tile_size };
+Vector2 player_pos{ tile_size * 8 , tile_size * 3 };
+int player_Speed = 1;
+Vector2 Shoot_dir;
 
 int fire_frame_counter = 0;
 int fire_rate = 15;
 
 //animation
 int animation_frame_counter = 0;
+
+bool bush_frame = 0;
+
+int damage = 1;
+
+//clock
+float timer_max = 10;
+float timer_current = timer_max;
+
+//death and start screen
+bool isDead = false;
+bool gameStarted = false;
+
+//life counter
+int player_life = 1;
 
 //bush
 /*
@@ -39,6 +61,32 @@ int player_Speed = 1;
 Vector2 Shoot_dir;
 int damage = 1;*/
 
+//Hierarchy
+class GameObject {
+private:
+public:
+	Vector2 position;
+	Vector2 direction;
+	int speed;
+};
+
+class player_character : GameObject {
+public:
+	Vector2 shoot_dir;
+};
+
+class Bullet : GameObject {
+	int damage;
+};
+
+class enemy : GameObject {
+	int hp;
+};
+
+class orc : enemy {
+
+};
+
 struct bullet {
 	int speed = 5;
 	Vector2 velocity;
@@ -46,7 +94,7 @@ struct bullet {
 	int damage;
 };
 
-struct enemy {
+struct trial_enemy {
 	int speed = 5;
 	Vector2 velocity;
 	Vector2 position;
@@ -64,15 +112,131 @@ struct obstacle obs;
 std::vector<bullet> bullet_tracker{};
 std::vector<bullet> bullet_pool{};
 
+std::vector<enemy> enemy_tracker{};
 std::vector<enemy> enemy_pool{};
-std::vector<enemy> enemy_pool{};
+
+//start screen
+Texture2D titleScreen;
+
+//player character
+Texture2D player_character_spritesheet;
+Texture2D player_character_death;
+Texture2D bullet_player;
+
+//enemies
+Texture2D orc_spritesheet;
+Texture2D troll_spritesheet;
+Texture2D spyke_ball_spritesheet;
+Texture2D mushroom_spritesheet;
+Texture2D butterfly_spritesheet;
+Texture2D mummie_spritesheet;
+Texture2D imp_spritesheet;
+
+//cowboy
+Texture2D cowboy_spritesheet;
+Texture2D cowboy_dialog;
+
+//enemy damage & death animation
+Texture2D damaged_butterfly;
+Texture2D damaged_cowboy;
+Texture2D damaged_imp;
+Texture2D damaged_mummie;
+Texture2D damaged_mushroom;
+Texture2D damaged_troll;
+Texture2D damaged_spyke_ball;
+Texture2D death_animation_dungeon;
+Texture2D death_animation_enemy;
+Texture2D flyer_death_animation;
+
+//shop
+Texture2D ammunition;
+Texture2D guns;
+Texture2D boots;
+Texture2D shop_jeeper_spritesheet;
+Texture2D shop_blanket;
+
+//general terrain
+Texture2D bridge;
+
+//desert terrain
+Texture2D dirt_grass;
+Texture2D dirt;
+Texture2D dirt_stones;
+Texture2D path;
+Texture2D path_stones;
+Texture2D river_desert;
+Texture2D bush_spritesheet;
+Texture2D logs;
+
+//forest terrain
+Texture2D grass;
+Texture2D flowers_grass;
+Texture2D path_grass;
+Texture2D path_grass2;
+Texture2D tall_grass;
+Texture2D river_forest;
+Texture2D green_bush_spritesheet;
+Texture2D stump;
+
+//cementery terrain
+Texture2D cementery_floor;
+Texture2D cementery_gravel;
+Texture2D cementery_path;
+Texture2D stone_floor;
+Texture2D tombstone;
+
+//power ups
+Texture2D coffe;
+Texture2D coin;
+Texture2D coin_5;
+Texture2D extra_life;
+
+//UI
+Texture2D power_blank;
+
+//Texture2D clock;
+Texture2D clock_;
+
+
+void LoadAssets() {
+	//load all assets here please!!
+	dirt_grass = LoadTexture("dirt_grass.png");
+	dirt = LoadTexture("dirt.png");
+	bush_spritesheet = LoadTexture("bush_spritesheet");
+	path = LoadTexture("path.png");
+	bridge = LoadTexture("bridge.png");
+	bullet_player = LoadTexture("bullet.png");
+	titleScreen = LoadTexture("title_screen.png");
+	extra_life = LoadTexture("extra_life.png");
+	coin = LoadTexture("coin.png");
+	power_blank = LoadTexture("power_up_blank.png");
+	clock_ = LoadTexture("clock.png");
+
+}
+void UnloadGame() {
+	UnloadTexture(dirt_grass);
+	UnloadTexture(dirt);
+	UnloadTexture(bush_spritesheet);
+	UnloadTexture(path);
+	UnloadTexture(clock_);
+	UnloadTexture(power_blank);
+	UnloadTexture(extra_life);
+	UnloadTexture(titleScreen);
+
+}
 
 void InitGame() {
 	SetTargetFPS(60);
 	obs.color = RED;
 	obs.position = Vector2{ tile_size * 8, tile_size * 8 };
+	LoadAssets();
 
 }
+
+void RestartGame();
+void ShowStartScreen();
+void ShowDeathScreen();
+
 void UpdateGame() {//update variables and positions
 	//command with keys
 
@@ -91,15 +255,15 @@ void UpdateGame() {//update variables and positions
 	}
 
 	//x axis limits
-	if (player_pos.x > 240) {
-		player_pos = { 240, player_pos.y };
+	if (player_pos.x > area_size - tile_size) {
+		player_pos = { area_size - tile_size, player_pos.y };
 	}
 	else if(player_pos.x < 0) {
 		player_pos = { 0, player_pos.y };
 	}
 	//y axis limits
-	if (player_pos.y > 240) {
-		player_pos = { player_pos.x, 240 };
+	if (player_pos.y > area_size - tile_size) {
+		player_pos = { player_pos.x, area_size - tile_size };
 	}
 	else if (player_pos.y < 0) {
 		player_pos = { player_pos.x, 0 };
@@ -115,19 +279,19 @@ void UpdateGame() {//update variables and positions
 	if (IsKeyDown(KEY_UP)) {
 		Shoot_dir = { Shoot_dir.x, -1 };
 	}
-	else if (IsKeyDown('D')) {
+	else if (IsKeyDown(KEY_DOWN)) {
 		Shoot_dir = { Shoot_dir.x , 1 };
 	}
 
-	if (IsKeyDown('S')) {
+	if (IsKeyDown(KEY_RIGHT)) {
 		Shoot_dir = { 1, Shoot_dir.y };
 	}
-	else if (IsKeyDown('W')) {
+	else if (IsKeyDown(KEY_LEFT)) {
 		Shoot_dir = { -1, Shoot_dir.y };
 	}
 
 	//create bullets
-	if ((Shoot_dir - x != 0 || Shoot_dir.y != 0) && frame_counter % fire_rate == 0) {
+	if ((Shoot_dir.x != 0 || Shoot_dir.y != 0) && fire_frame_counter % fire_rate == 0) {
 		if (bullet_pool.empty()) {
 			struct bullet b;
 			b.damage = damage;
@@ -187,11 +351,63 @@ void UpdateGame() {//update variables and positions
 		}
 
 	}
+
+	animation_frame_counter++;
+
+	if (animation_frame_counter >= 60) {
+		animation_frame_counter = 0;
+		bush_frame = !bush_frame;
+	}
 }
+
+void DrawMap() {
+	char T;
+	ifstream area("AREAS/area1_1.txt");
+	Rectangle src;
+	if (bush_frame == 0) {
+		src = { 0.0f, 0.0f, 16.0f, 16.0f };
+	}
+	else {
+		src = { 17.0f, 0.0f, 32.0f, 16.0f };
+	}
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 16; j++) {
+			area.get(T);
+			switch (T) {
+			case 'D':
+				DrawTextureEx(dirt, { tile_size * j, tile_size * i }, 0, tile_size / 16, WHITE);
+				break;
+			case 'P':
+				DrawTextureEx(path, { tile_size * j, tile_size * i }, 0, tile_size / 16, WHITE);
+				break;
+			case 'B':
+				//DrawTextureEx(bush_spritesheet, { tile_size * j, tile_size * i }, 0, tile_size / 16, WHITE);
+				//DrawTexturePro(bush_spritesheet, src, { 0.0f, 0.0f, tile_size, tile_size }, { tile_size * j, tile_size * i }, 0, WHITE);
+				//DrawTexturePro(bridge, { 0.0f, 0.0f, 16.0f, 16.0f }, { 0.0f, 0.0f, tile_size, tile_size }, { tile_size * j, tile_size * i }, 0, WHITE);
+				//DrawTexturePro(bridge, { 0.0f, 0.0f, 16.0f, 16.0f }, { tile_size * j, tile_size * i }, {tile_size, tile_size }, 0, WHITE);
+
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
 void DrawGame() {//draws the game every frame
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
+	DrawMap();
 	DrawRectangleV(player_pos, player_size, BLACK);
+	if (obs.alive) {
+		DrawRectangleV(obs.position, {tile_size, tile_size}, obs.color);
+	}
+
+	int bullet_amount = bullet_tracker.size();
+	for (int i = 0; i < bullet_amount; i++) {
+		DrawTextureEx(bullet_player, bullet_tracker[i].position, 0, tile_size / 16, WHITE);
+	}
+
 	EndDrawing();
 }
 void UpdateDrawFrame() {
@@ -206,23 +422,17 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(256, 256, "Hello Raylib");
+	InitWindow(area_size, area_size, "Journey of the Prairie King");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
-
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
-	
+	InitGame();
 
 	while (!WindowShouldClose()) {
 		UpdateDrawFrame();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
